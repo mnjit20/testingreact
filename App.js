5a202f8a61456bfaabee3a3fab94e5e0e73167a4
Manjeet Verma <manver8@dainikbhaskargroup.com>
2018-01-30 22:16:19 +0530

    intial

---
+++ App.js
@@ -0,0 +1,69 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+/*
+import {
+  AdMobBanner,
+  AdMobInterstitial,
+  PublisherBanner,
+  AdMobRewarded,
+} from 'react-native-admob'
+
+*/
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu', 
+});
+
+
+const ads = '300x250 ad test 2.1 manjeet 111' ;
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+            Dainik Bhaskar
+        </Text>
+        <Text style={styles.welcome}>
+            Testing ReactNative on android
+        </Text>
+          <Text style={styles.instructions}>
+          {ads}
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
